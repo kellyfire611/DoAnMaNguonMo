557c1013d92d285ead1aeb8e6581527b901489a3
kellyfire <kellyfire611@gmail.com>
2019-03-25 22:39:14 +0700

    Bổ sung báo cáo

--- app/Http/Controllers/Backend/DashboardController.php
+++ app/Http/Controllers/Backend/DashboardController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Backend;
 use App\Http\Controllers\Controller;
 use App\Models\DiaDiem;
 use App\Models\TinhThanh;
+use App\Models\QuangCao;
 use App\Models\Auth\User;
 
 /**
@@ -33,7 +34,34 @@ class DashboardController extends Controller
         $tinhthanhs = TinhThanh::all();
         $baocao['tinhthanh_count'] = $tinhthanhs->count();
 
+        $tongsoluotdanhgia = 0;
+        foreach($diadiems as $diadiem)
+        {
+            $diem = 0;
+            $sumDiem = 0;
+            foreach($diadiem->danhgias()->get() as $key=>$value)
+            {
+                $sumDiem += empty($value->diem) ? 0 : $value->diem;
+            }
+            if($diadiem->danhgias()->count() <= 0)
+            {
+                $diadiem->diemtrungbinh = 0;
+            }
+            else
+            {
+                $diadiem->diemtrungbinh = $sumDiem / $diadiem->danhgias()->count();
+            }
+            $tongsoluotdanhgia += $diadiem->danhgias()->count();
+        }
+        
+        $quangcaos = QuangCao::all();
+
+        $top5diadiems = $diadiems->sortByDesc('diemtrungbinh')->take(5);
+
         return view('backend.dashboard')
-            ->with('baocaodata', $baocao);
+            ->with('baocaodata', $baocao)
+            ->with('top5diadiems', $top5diadiems)
+            ->with('quangcaos', $quangcaos)
+            ->with('tongsoluotdanhgia', $tongsoluotdanhgia);
     }
 }
